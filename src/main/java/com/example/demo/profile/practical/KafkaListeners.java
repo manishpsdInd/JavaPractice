e09870e4a452f05f4449084cc0998a610fbd30ad
Prasad <39470551+manishpsdInd@users.noreply.github.com>
2024-08-23 16:41:49 +0530

    Update KafkaListeners.java

--- src/main/java/com/example/demo/profile/practical/KafkaListeners.java
+++ src/main/java/com/example/demo/profile/practical/KafkaListeners.java
@@ -1,54 +1,8 @@
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.listener.adapter.RecordFilterStrategy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaListeners {
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(
-            ConsumerFactory<String, String> consumerFactory) {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory);
-        return factory;
-    }
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactoryForValue1(
-            ConsumerFactory<String, String> consumerFactory) {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory);
-        factory.setRecordFilterStrategy(new RecordFilterStrategy<String, String>() {
-            @Override
-            public boolean filter(ConsumerRecord<String, String> consumerRecord) {
-                String headerValue = new String(consumerRecord.headers().lastHeader("headerKey").value());
-                return !"value1".equals(headerValue); // Filter out if header value is not "value1"
-            }
-        });
-        return factory;
-    }
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactoryForValue2(
-            ConsumerFactory<String, String> consumerFactory) {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory);
-        factory.setRecordFilterStrategy(new RecordFilterStrategy<String, String>() {
-            @Override
-            public boolean filter(ConsumerRecord<String, String> consumerRecord) {
-                String headerValue = new String(consumerRecord.headers().lastHeader("headerKey").value());
-                return !"value2".equals(headerValue); // Filter out if header value is not "value2"
-            }
-        });
-        return factory;
-    }
 
     @KafkaListener(topics = "your-topic-name", groupId = "group1", containerFactory = "kafkaListenerContainerFactoryForValue1")
     public void listener1(String message) {
